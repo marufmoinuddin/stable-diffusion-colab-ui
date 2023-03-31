39fdf8cbe507f518fa246fb4f8da2590264e6e76
Moin Uddin Amed <56315433+marufmoinuddin@users.noreply.github.com>
2023-03-31 06:21:41 +0600

    Create publish.sh

---
+++ publish.sh
@@ -0,0 +1,9 @@
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+./ngrok authtoken $CRP 
+nohup ./ngrok http 1234 &>/dev/null &
+echo Your Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "Note: Use Right-Click Or Ctrl+C To Copy"
+echo "Please Keep Colab Tab Open, Maximum Time 12h"
