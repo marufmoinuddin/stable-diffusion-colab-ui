f58a8d192c0d03ec82b61be08f9d6c65dd8f443e
Moin Uddin Amed <56315433+marufmoinuddin@users.noreply.github.com>
2023-03-31 06:49:39 +0600

    Update publish-local-folder.sh

--- publish-local-folder.sh
+++ publish-local-folder.sh
@@ -4,7 +4,7 @@ read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok http 1234 &>/dev/null &
 echo Your Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p'
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
 sh /content/drive/MyDrive/Projects/easy-diffusion/start.sh  > /dev/null 2>&1
